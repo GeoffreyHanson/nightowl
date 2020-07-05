749fd856a1f287c976b70623849a75ff9f93ee1f
Geoffrey Hanson <g.hanson.b@gmail.com>
2020-07-04 21:10:13 -0500

    Successfully tested displaying mock data with state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './components/Title/Title';
 import SearchBar from './components/SearchBar/SearchBar';
 import CafeList from './components/CafeList/CafeList';
 import './App.css';
 
 function App() {
+  const [cafes, setCafes] = useState([
+    {
+      id: 1,
+      name: 'Spyhouse',
+      distance: 2,
+      close: '11:00pm',
+    },
+    {
+      id: 2,
+      name: 'Dogwood',
+      distance: 1,
+      close: '8:00pm',
+    },
+  ]);
+
+  // const cafes = [
+  //   {
+  //     id: 1,
+  //     name: 'Spyhouse',
+  //     distance: 2,
+  //     close: '11:00pm',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Dogwood',
+  //     distance: 1,
+  //     close: '8:00pm',
+  //   },
+  // ];
   return (
     <div className="App">
       <Title />
       <SearchBar />
-      <CafeList />
+      <CafeList cafes={cafes} />
     </div>
   );
 }
