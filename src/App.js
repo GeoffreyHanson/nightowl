35a63ccf44dcac6781897e9aa07e88d87850be4a
Geoffrey Hanson <g.hanson.b@gmail.com>
2020-07-29 21:06:29 -0500

    Details are now successfully retrieved and displayed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Title from './components/Title/Title';
 import SearchBar from './components/SearchBar/SearchBar';
 import CafeList from './components/CafeList/CafeList';
@@ -8,78 +8,24 @@ import './App.css';
 const App = () => {
   const [cafes, setCafes] = useState([]);
 
-
-  // Should update when cafes[0].closingTime changes
-  // if cafes[0].closingTime, then
-  // function handleChange() {
-  //   // if (cafes[0].closingTime) {
-  //   const updatedCafes = cafes.map((cafe) => {
-  //     cafe.overnight = false;
-  //     cafe.closingTime = '1200';
-  //     return cafe;
-  //   });
-  //   console.log('Updated...');
-  //   console.log(updatedCafes);
-  //   // Currently causes an infinite loop...
-  //   setCafes(updatedCafes);
-  //   // }
-  // }
-
-
-  // useEffect(() => {
-  //   if (cafes.length !== 0) {
-  //     const updatedCafes = cafes.map((cafe) => {
-  //       cafe.overnight = false;
-  //       cafe.closingTime = '1200';
-  //       return cafe;
-  //     });
-  //     console.log('Updated...');
-  //     console.log(updatedCafes);
-  //     // Currently causes an infinite loop...
-  //     setCafes(updatedCafes);
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   if (cafes.length !== 0) {
-  //     cafes.map((cafe) => {
-  //       Yelp.details(cafe.id).then((details) => {
-  //         cafe.overnight = details.overnight;
-  //         cafe.closingTime = details.closingTime;
-  //       });
-  //     });
-  //     // console.log(updatedCafes);
-  //     console.log(cafes);
-  //     console.log('Updated');
-  //   }
-  // }, [cafes]);
+  function getDetails(cafesToDetail) {
+    Promise.all(cafesToDetail.map((cafe) => Yelp.details(cafe.id)
+      .then((cafeDetails) => {
+        const detailedCafe = {
+          ...cafe,
+          closingTime: cafeDetails.closingTime,
+          overnight: cafeDetails.overnight,
+        };
+        return detailedCafe;
+      }))).then((detailedCafes) => setCafes(detailedCafes));
+  }
 
 
   const searchYelp = (location) => {
     Yelp.search(location).then((cafeList) => {
-      const cafeArr = [];
-      cafeList.forEach((cafe) => {
-        Yelp.details(cafe.id).then((details) => {
-          // cafe.overnight = details.overnight;
-          // cafe.closingTime = details.closingTime;
-          cafe = {
-            ...cafe,
-            overnight: details.overnight,
-            closingTime: details.closingTime,
-          };
-          // console.dir(cafeContainer);
-          cafeArr.push(cafe);
-          // return cafe;
-          // setCafes(where: cafe.id)
-          // How do I add container to the cafe
-        });
-        // .then((result) => result);
-        // return container;
-        // console.dir(detailedCafe);
-      });
-      console.table(cafeArr);
-      // console.log(cafeList);
+      // console.table(cafeList);
       // setCafes(cafeList);
+      getDetails(cafeList);
     });
     // For each cafe in cafes
   };
@@ -91,7 +37,7 @@ const App = () => {
       <SearchBar searchYelp={searchYelp} />
       <CafeList
         cafes={cafes}
-        setCafes={setCafes}
+        // setCafes={setCafes}
       />
     </div>
   );
